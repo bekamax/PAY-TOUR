3807ef1e74542874a2f29d87773c38c2ece4b914
bekamax <bekamax05@gmail.com>
2024-07-16 16:40:59 +0600

    Beka

--- JS/main-signup.js
+++ JS/main-signup.js
@@ -3,47 +3,62 @@ const signupForm = document.querySelector("#signup-form");
 const email = document.querySelector("#email");
 const passwordInputSignup = document.querySelector("#password");
 
-// registration logic
-
+// URL для регистрации
 const url = "https://api.worldota.net/api/b2b/v3/profiles/create/";
-const apiKey = "key_id:key";
+
+// Ваш производственный ключ API
+const keyId = "7300";
+const key = "5d601122-cce8-4979-90a4-945ff0de0d09";
 
 if (signupForm) {
   signupForm.addEventListener("submit", async (event) => {
     event.preventDefault();
-    await registration(passwordInputSignup, email);
+    await registration(email, passwordInputSignup);
   });
 }
 
-async function registration(passwordInputSignup, email) {
-  const headers = new Headers();
-  headers.append("Content-Type", "application/json");
-  headers.append("Authorization", "Basic " + btoa(apiKey));
+const encodedApiKey = btoa(`${keyId}:${key}`);
+console.log(`Encoded API Key: ${encodedApiKey}`); // Логирование закодированного ключа API
+
+async function registration(email, passwordInputSignup) {
+  const headers = {
+    "Content-Type": "application/json",
+    Authorization: `Basic ${encodedApiKey}`,
+  };
 
   if (passwordInputSignup.value.length < 8) {
     alert("Password must be more than 8 characters!");
     return;
   }
-  let raw = JSON.stringify({
+
+  let raw = {
     email: email.value,
     password: passwordInputSignup.value,
-  });
-  console.log(raw);
+  };
+
+  console.log("URL: ", url); // Логирование URL
+  console.log("Request Headers: ", headers); // Логирование заголовков запроса
+  console.log("Request Body: ", raw);
 
   try {
     const res = await fetch(url, {
       method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      // mode: "no-cors",
-      body: raw,
-      redirect: "follow",
+      headers: headers,
+      mode: "no-cors",
+      body: JSON.stringify(raw),
     });
-    if (res.ok) {
-      alert("User successfully registrated!");
+
+    if (!res.ok) {
+      const errorText = await res.text();
+      console.error("Error response:", errorText);
+      throw new Error(`Error: ${res.status} ${res.statusText}`);
     }
+
+    const data = await res.json();
+    console.log("Регистрация прошла успешно:", data);
+    alert("User successfully registered!");
   } catch (error) {
-    console.error(`Error cought: ${error}`);
+    console.error(`Error caught: ${error}`);
+    alert(`Error: ${error.message}`);
   }
 }
